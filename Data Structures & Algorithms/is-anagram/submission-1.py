a3ad52cf3fab24e7ac81b1e855c826e56f418e43
Gibran Myageri <gibranmyageri@gmail.com>
2026-08-11 20:58:01 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        sMap = {}
+        tMap = {}
+        for c in s:
+            sMap[c] = 1 + sMap.get(c, 0)
+        for c in t:
+            tMap[c] = 1 + tMap.get(c, 0)
+        if sMap == tMap:
+            return True
+        return False
